18ae8ff95b2a7f10216406ac0c9da1f0590c22fa
anmei <1298160604@qq.com>
2015-05-04 18:14:45 +0800

    fds

--- src/com/rhcheng/test/MainTest.java
+++ src/com/rhcheng/test/MainTest.java
@@ -1,5 +1,6 @@
 package com.rhcheng.test;
 
+import java.applet.AppletContext;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -8,10 +9,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.faces.application.Application;
+
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.SerializationConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.rhcheng.user.entity.User;
 
@@ -19,35 +24,41 @@ import com.rhcheng.user.entity.User;
 public class MainTest {
 	
 	public static void main(String[] args) throws JsonGenerationException, JsonMappingException, IOException{
-		User user=new User(); //Java Object
-		user.setName("hah虎");
-		user.setAddress("fds");
-		user.setUserid(1L);
-		user.setBirthday(new Date());
+		ApplicationContext ac = new ClassPathXmlApplicationContext("/configure/applicationContext.xml");
+		TestDI td = ac.getBean("testDI", TestDI.class);
+		td.print();
 		
-		Map<String,Object> m = new HashMap<String,Object>();
-		m.put("a", "abc");
-		m.put("b", user);
 		
-		List<Object> li = new ArrayList<Object>();
-		li.add(m);
-		li.add(user);
-		li.add("fsd");
-
-		ObjectMapper mapper = new ObjectMapper();
-		/** 时间格式等配置*/
-		mapper.configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
-		mapper.getSerializationConfig().setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));  
 		
-		String jsonstr = mapper.writeValueAsString(li); //返回字符串
-		System.out.println(jsonstr);
-		
-		List ma = mapper.readValue(jsonstr, ArrayList.class);
-		
-		Map a = (Map)ma.get(0);
-		/** POJO也转为map基本类型了*/
-		Map b = (Map)a.get("b");
-		System.out.println(b.get("birthday")+" "+b.get("name"));
+//		User user=new User(); //Java Object
+//		user.setName("hah虎");
+//		user.setAddress("fds");
+//		user.setUserid(1L);
+//		user.setBirthday(new Date());
+//		
+//		Map<String,Object> m = new HashMap<String,Object>();
+//		m.put("a", "abc");
+//		m.put("b", user);
+//		
+//		List<Object> li = new ArrayList<Object>();
+//		li.add(m);
+//		li.add(user);
+//		li.add("fsd");
+//
+//		ObjectMapper mapper = new ObjectMapper();
+//		/** 时间格式等配置*/
+//		mapper.configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
+//		mapper.getSerializationConfig().setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));  
+//		
+//		String jsonstr = mapper.writeValueAsString(li); //返回字符串
+//		System.out.println(jsonstr);
+//		
+//		List ma = mapper.readValue(jsonstr, ArrayList.class);
+//		
+//		Map a = (Map)ma.get(0);
+//		/** POJO也转为map基本类型了*/
+//		Map b = (Map)a.get("b");
+//		System.out.println(b.get("birthday")+" "+b.get("name"));
 		
 		
 		
---
+++ src/com/rhcheng/test/TestDI.java
@@ -0,0 +1,19 @@
+package com.rhcheng.test;
+
+
+public class TestDI {
+	private String a;
+
+	public String getA() {
+		return a;
+	}
+
+	public void setA(String a) {
+		this.a = a;
+	}
+	
+	public void print(){
+		System.out.println(a);
+	}
+	
+}
